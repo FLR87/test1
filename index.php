51e8a5a37133fccb8360c4bad4c798fb8ef17378
FLR87 <58577Kp6>
2018-01-05 20:54:14 +0200

    MVC_test1

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
 
-echo "ok dude, it's all ok";
+//FRONT CONTROLLER
 
+//1.Общие настройки
+
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+
+//2.Подключение файловой системы
+define('ROOT',dirname(__FILE__));
+require_once(ROOT . '/components/Router.php');
+
+//3.Установка соединения с БД
+
+//4.Вызов router
+$router = new Router();
+$router->run();
 
 ?>
