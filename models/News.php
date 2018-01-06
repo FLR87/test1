b73dbdedde8598fb48ad67a99d0c27fd29966cb4
FLR87 <58577Kp6>
2018-01-06 21:48:59 +0200

    MVC_test1_css_mudilovo

--- models/News.php
+++ models/News.php
@@ -21,11 +21,11 @@ class News
 
     public static function getNewsList()
     {
-//        $db = Db::getConnection();
+        $db = Db::getConnection();
 
         $newsList = array();
 
-        $result = $db->query("SELECT id, title, date, short_content FROM news ORDER BY date DESC LIMIT 10");
+        $result = $db->query("SELECT id, title, date, short_content, author_name FROM news ORDER BY date DESC LIMIT 10");
 
         $i = 0;
         while ($row = $result->fetch()) {
@@ -33,6 +33,7 @@ class News
             $newsList[$i]['title'] = $row['title'];
             $newsList[$i]['date'] = $row['date'];
             $newsList[$i]['short_content'] = $row['short_content'];
+            $newsList[$i]['author_name'] = $row['author_name'];
             $i++;
         }
 
--- views/news/index.php
+++ views/news/index.php
@@ -5,7 +5,8 @@
     <title>Culinary by FCT</title>
     <meta name="keywords" content=""/>
     <meta name="description" content=""/>
-    <link href="template/css/style.css" rel="stylesheet" type="text/css"/>
+    <link href="../template/css/style.css" rel="stylesheet" type="text/css"  media="screen" />
+<!--    <link href="/template/css/style.css" rel="stylesheet" type="text/css"/>-->
 </head>
 <body>
 <div id="wrapper">
@@ -29,64 +30,20 @@
     <div id="page">
         <div id="page-bgtop">
             <div id="content">
+
+               <?php foreach ($newsList as $newsItem):?>
                 <div class="post">
-                    <h2 class="title"><a href="#">Lorem Ipsum Dolor Volutpat</a></h2>
-                    <p class="byline">Posted by FreeCssTemplates</p>
+                    <h2 class="title"><a href="/news/<?php echo $newsItem['id'];?>"><?php echo $newsItem['title'];?></a></h2>
+                    <p class="byline">Published by <?php echo $newsItem['author_name'];?></p>
                     <div class="entry">
-                        <p>This is <strong>Culinary</strong>, a free, fully standards-compliant CSS template designed by
-                            <a href="http://www.freecsstemplates.org">FCT</a>, released for free under the <a
-                                    href="http://creativecommons.org/licenses/by/2.5/">Creative Commons Attribution
-                                2.5</a> license. The photo used in this template is from <a
-                                    href="http://www.pdphoto.org">PDPhoto.org</a>. You're free to use this template for
-                            anything as long as you link back to <a href="http://www.freecsstemplates.org/">my site</a>.
-                            Enjoy :)</p>
+                        <p><?php echo $newsItem['short_content'];?></p>
                     </div>
                     <div class="meta">
-                        <p class="links"><a href="#" class="comments">Comments (32)</a></p>
+                        <p class="links"><a href="/news/<?php echo $newsItem['id'];?>" class="comments">Comments (32)</a></p>
                     </div>
                 </div>
-                <div class="post">
-                    <h2 class="title"><a href="#">Lorem Ipsum Dolor Volutpat</a></h2>
-                    <p class="byline">Posted by FreeCssTemplates</p>
-                    <div class="entry">
-                        <p>Curabitur tellus. Phasellus tellus turpis, iaculis in, faucibus lobortis, posuere in, lorem.
-                            Donec a ante. Donec neque purus, adipiscing id, eleifend a, cursus vel, odio. Vivamus varius
-                            justo sit amet leo. Morbi sed libero. Vestibulum blandit augue at mi. Praesent fermentum
-                            lectus eget diam. Nam cursus, orci sit amet porttitor iaculis, ipsum massa aliquet nulla,
-                            non elementum mi elit a mauris.</p>
-                    </div>
-                    <div class="meta">
-                        <p class="links"><a href="#" class="comments">Comments (32)</a></p>
-                    </div>
-                </div>
-                <div class="post">
-                    <h2 class="title"><a href="#">Praesent scelerisque scelerisque</a></h2>
-                    <p class="byline">Posted by FreeCssTemplates</p>
-                    <div class="entry">
-                        <p>Curabitur tellus. Phasellus tellus turpis, iaculis in, faucibus lobortis, posuere in, lorem.
-                            Donec a ante. Donec neque purus, adipiscing id, eleifend a, cursus vel, odio. Vivamus varius
-                            justo sit amet leo. Morbi sed libero. Vestibulum blandit augue at mi. Praesent fermentum
-                            lectus eget diam. Nam cursus, orci sit amet porttitor iaculis, ipsum massa aliquet nulla,
-                            non elementum mi elit a mauris.</p>
-                    </div>
-                    <div class="meta">
-                        <p class="links"><a href="#" class="comments">Comments (32)</a></p>
-                    </div>
-                </div>
-                <div class="post">
-                    <h2 class="title"><a href="#">Fusce ultrices fringilla</a></h2>
-                    <p class="byline">Posted by FreeCssTemplates</p>
-                    <div class="entry">
-                        <p>Curabitur tellus. Phasellus tellus turpis, iaculis in, faucibus lobortis, posuere in, lorem.
-                            Donec a ante. Donec neque purus, adipiscing id, eleifend a, cursus vel, odio. Vivamus varius
-                            justo sit amet leo. Morbi sed libero. Vestibulum blandit augue at mi. Praesent fermentum
-                            lectus eget diam. Nam cursus, orci sit amet porttitor iaculis, ipsum massa aliquet nulla,
-                            non elementum mi elit a mauris.</p>
-                    </div>
-                    <div class="meta">
-                        <p class="links"><a href="#" class="comments">Comments (32)</a></p>
-                    </div>
-                </div>
+               <?php endforeach;?>
+
             </div>
             <!-- end div#content -->
             <div id="sidebar">
